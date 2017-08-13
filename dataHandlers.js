fce56a27bbf43f5e52a076bf7c6dbf6ec265e50c
Patricio Torres <patricio.torres.ce@gmail.com>
2017-08-13 09:47:12 -0700

    JSON Files

--- dataHandlers.js
+++ dataHandlers.js
@@ -28,8 +28,37 @@ d3.text("data.txt")
 
 
 d3.xml("data.xml").get(function(error,data){
-  console.log(data);
+  var xmlLetter = data.documentElement.getElementsByTagName("letter");
+  var letterNodes = d3.select(data).selectAll("letter")._groups[0][0];
+  //console.log(letterNodes);
 });
+
+d3.text("test.txt").get(function(error,data){
+
+  var myTabPositions = [];
+  var myNewLinePositions = [];
+
+  var tabVal = '\\b\t\\b';
+  var tabMod = 'g';
+  var tabRegExp = new RegExp(tabVal,tabMod);
+
+  var lineVal = '\\b\n\\b';
+  var lineMod = 'g';
+  var lineRegExp = new RegExp(lineVal,lineMod);
+
+  data.replace(tabRegExp,function(a,b){myTabPositions.push(b); return a;});
+  data.replace(lineRegExp,function(a,b){myNewLinePositions.push(b); return a;});
+
+  // console.log(myTabPositions);
+  // console.log(myNewLinePositions);
+
+});
+
+d3.html("https://enable-cors.org").get(function(error,data) {
+  var frag = data.querySelector("div");
+    console.log(frag);
+});
+
 
 d3.json("treeData.json").get(function(error, data){
 
